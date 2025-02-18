465acba25b09a02f244751b07dfe798ec124eaa7
Rohan Patel <patelrohan@users.noreply.github.com>
2025-02-18 00:16:27 -0500

    Initial Commit

---
+++ BestRecipes/BestRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestRecipesApp.swift
+//  BestRecipes
+//
+//  Created by Rohan Patel on 2/18/25.
+//
+
+import SwiftUI
+
+@main
+struct BestRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
