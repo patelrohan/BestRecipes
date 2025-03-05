846f0542df690a1798d52fa6ec4daed1b6928056
Rohan Patel <patelrohan@users.noreply.github.com>
2025-03-04 20:57:08 -0500

    Unit test for API calls and image caching
    Added a local JSON file for future where only JSON Decoding logic can be tested to avoid including network call logic in testing

--- BestRecipesTests/BestRecipesTests.swift
+++ BestRecipesTests/BestRecipesTests.swift
@@ -18,19 +18,33 @@ final class BestRecipesTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testfetchRecipes() async throws{
+        do{
+            let recipes = try await NetworkManager.shared.fetchRecipes()
+            XCTAssertTrue(recipes.count > 0, "Expected at least 1 recipe")
+        }catch{
+            XCTFail("Fetchin recipes failed with error: \(error)")
         }
     }
-
+    
+    func testImageDownloadAndCacheSucess() async throws {
+        do{
+            let imageUrl = "https://d3jbb8n5wk0qxi.cloudfront.net/photos/535dfe4e-5d61-4db6-ba8f-7a27b1214f5d/small.jpg"
+            let image = try await NetworkManager.shared.downloadImage(fromUrl: imageUrl)
+            XCTAssertNotNil(image, "Unsuccessful image download")
+            
+            let cachedImage = NetworkManager.shared.retrieveImageFromCache(location: imageUrl)
+            XCTAssertNotNil(cachedImage, "Unsuccessful image retrieval from Cache.")
+        }catch{
+            XCTFail("Downloading recipe image failed with error: \(error)")
+        }
+    }
+    
+    func testImageRetrievalFromCacheFailure() {
+            let imageUrl = "https://d3jbb8n5wk0qxi.cloudfront.net/photos/535dfe4e-5d61-4db6-ba8f-7a27b1214f5d/large.jpg"
+            let cachedImage = NetworkManager.shared.retrieveImageFromCache(location: imageUrl)
+            XCTAssertNil(cachedImage, "Unsuccessful. Image should not be in cache.")
+    }
+    
 }
